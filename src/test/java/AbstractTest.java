2db9e3330fe5b4cb9ce187b3a99e94e72648e625
Mikael Larsson <mikael.p.larsson@epsilon.nu>
2015-02-10 08:49:01 +0100

    AbstractTest is now a master helper class.

--- src/test/java/AbstractTest.java
+++ src/test/java/AbstractTest.java
@@ -1,15 +1,25 @@
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.impl.SimpleLogger;
+import se.romram.handler.TestHandler;
+import se.romram.server.RelaxServer;
+
+import java.io.IOException;
 
 /**
  * Created by micke on 2015-02-08.
  */
 public class AbstractTest {
     protected Logger log = LoggerFactory.getLogger(RelaxServerTest.class);
+	protected static int port = 2357;
+	protected static String protocol = "http://";
+	protected static String domain = "localhost";
+	protected static String url = String.format("%s%s:%s", protocol, domain, port);
 
     @Rule
     public TestRule testRule = new TestRule() {
@@ -25,6 +35,26 @@ public class AbstractTest {
             };
         }
     };
+
+	@BeforeClass
+	public static void beforeClass() throws IOException {
+		System.setProperty(SimpleLogger.DEFAULT_LOG_LEVEL_KEY, "info");
+		System.setProperty(SimpleLogger.SHOW_DATE_TIME_KEY, "true");
+		System.setProperty(SimpleLogger.DATE_TIME_FORMAT_KEY, "dd HH:mm:ss.SSS");
+	}
+
+	protected String buildUrl(String path) {
+		return buildUrl(path, "");
+	}
+
+	protected String buildUrl(String path, String query) {
+		if (!query.isEmpty()) {
+			if (query.charAt(0) != '/') {
+				query = "/" + query;
+			}
+		}
+		return String.format("%s%s:%s%s", protocol, domain, port, path, query);
+	}
 
 
 }
